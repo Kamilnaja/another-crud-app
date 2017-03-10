2d138427e1259c77b31bdd4482d864afc02d385b
Kamil Naja <kamilnaja@gmail.com>
2017-03-11 00:22:39 +0100

    added additem to array

--- script.js
+++ script.js
@@ -23,47 +23,45 @@ var app = {
     },
 
     addItem: function () {
-        var newItem = document.createElement('span');
-        newItem.classList = 'listItem';
-        newItem.innerHTML = _('addInput').value;
-        newItem.id = _('addInput').value.replace(/\s+/g, '');
-        //wrapnij w span
-        _('list').appendChild(newItem);
-        var listItems = newItem.id;
-        $('#' + newItem.id).wrap('<li class="listItem"></li>').append('<select class="itemSelect"><option value="erase">Edit</option><option>Erase</option></select>');
-        $listItems = "";
-
+        var tempInput = $('#addInput').val();
+        var submitNewitem = $('#addBtn');
+           app.items.push(tempInput);
+           console.log(app.items);
         _('addInput').value = "";
+        app.displayItem();
     },
 
-    editItem: function (e) {
-        var editor = e.target;
-        //wyłącz przycisk edit
-        //dodaj input
-        //jeśli element nie ma inputa to dodaj
-        function checkAndEdit (event) {
-            if (editInput.value.length !== 0) {
-                console.log(e.target.id);
-                e.target.innerHTML = editInput.value;
-            } else {
-                console.log("wpisz coś");
-            }
-            event.stopPropagation();
-            $('#list span').on('click', app.editItem);
+    editItem: function () {
+        // var editor = e.target;
+        // //wyłącz przycisk edit
+        // //dodaj input
+        // //jeśli element nie ma inputa to dodaj
+        // function checkAndEdit (event) {
+        //     if (editInput.value.length !== 0) {
+        //         console.log(e.target.id);
+        //         e.target.innerHTML = editInput.value;
+        //     } else {
+        //         console.log("wpisz coś");
+        //     }
+        //     event.stopPropagation();
+        //     $('#list span').on('click', app.editItem);
+        // }
+        // $('#list span').off('click', app.editItem);
+        //
+        // var editInput = document.createElement('input');
+        // editInput.placeholder = 'edytuj';
+        // editInput.id = 'editInput';
+        // var editSubmit = document.createElement('button');
+        // //stwórz btn do potwierdzenia zmian
+        // editSubmit.innerHTML = "Edytuj";
+        // editSubmit.id = 'editSubmit';
+        // editor.appendChild(editInput);
+        // editor.appendChild(editSubmit);
+        // _('editSubmit').addEventListener('click', checkAndEdit);
+        // e.stopPropagation();
+        if ($('.itemSelect').val() == "edit"){
+            console.log('app is edited');
         }
-        $('#list span').off('click', app.editItem);
-
-        var editInput = document.createElement('input');
-        editInput.placeholder = 'edytuj';
-        editInput.id = 'editInput';
-        var editSubmit = document.createElement('button');
-        //stwórz btn do potwierdzenia zmian
-        editSubmit.innerHTML = "Edytuj";
-        editSubmit.id = 'editSubmit';
-        editor.appendChild(editInput);
-        editor.appendChild(editSubmit);
-        _('editSubmit').addEventListener('click', checkAndEdit);
-        e.stopPropagation();
     },
 
     deleteItem: function () {
@@ -77,6 +75,6 @@ var app = {
 
 app.displayItem();
 //event listeners
-_('addBtn').addEventListener('click', app.addItem);
-//also for dynamically created elements
+$('#addBtn').on('click', app.addItem);
 $(document).on('change', '.itemSelect', app.deleteItem);
+$(document).on('change', '.itemSelect', app.editItem);
