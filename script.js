ade72a2d8dad3dc1c6e60fceef1d1f1807f37b24
Kamil Naja <kamilnaja@gmail.com>
2017-04-19 01:01:29 +0200

    refactorisation

--- script.js
+++ script.js
@@ -1,84 +1,98 @@
 /**
  * Created by Kamil on 2017-03-08.
  */
-var app = {
-    saveToLocalStorage: function () {
-        localStorage.setItem('app.items', JSON.stringify(app.items));
-    },
-    getFromLocalStorage: function () {
-        var retrievedObject = localStorage.getItem('app.items');
+var app = (function () {
+    "use strict";
+    var _saveToLocalStorage = function () {
+        localStorage.setItem("app.items", JSON.stringify(app.items));
+    };
+    var _getFromLocalStorage = function () {
+        var retrievedObject = localStorage.getItem("app.items");
         app.items = JSON.parse(retrievedObject);
-    },
-    displayItem: function () {
-        app.getFromLocalStorage();
-        var data = "";
-        for (var i in app.items) {
-            var optionValue = `
-            <select class="itemSelect">
-                <option hidden value="unchosen">Options</option>
-                <option value="edit">Edit</option>
-                <option value="erase">Erase</option>
-            </select></li></span>`;
-            //nadaj kolejno id
-            data += '<li class="listItem"><span id=' + i + '>' + app.items[i].name + optionValue;
-        }
-        //dodaj wartości do pola
-        $('#list').html(data);
-    },
+    };
 
-    addItem: function () {
+    var _displayItem = function () {
+        _getFromLocalStorage();
+        var _data = "";
+        for (var i in app.items) {
+            var optionValue =
+                '<select class="itemSelect">' +
+                '<option hidden value="unchosen">Options</option>' +
+                '<option value="edit">Edit</option>' +
+                '<option value="erase">Erase</option>' +
+                '</select></li></span>';
+            //nadaj kolejno id
+            _data += '<li class="listItem"><span id=' + i + ">" + app.items[i].name + optionValue;
+        }
+        _list.innerHTML = _data;
+    };
+
+    var _addItem = function () {
         // jeśli localstorage jest pusta to dodaj do niej pusty element
-        if (localStorage.getItem('app.items') === null) {
+        if (localStorage.getItem("app.items") === null) {
             //zainicjuj
             app.items = [];
-            app.saveToLocalStorage();
+            _saveToLocalStorage();
         }
-        app.getFromLocalStorage();
-        // app.items = JSON.parse(localStorage.getItem('app.items'));
-        var tempInput = $('#addInput').val();
-        var submitNewitem = $('#addBtn');
-        //dodaj pusty object na koniec
-        app.items[app.items.length] = {};
-        app.items[app.items.length - 1].name = tempInput;
-        app.items[app.items.length - 1].isFinished = "false";
-        //zapisz do local storage
-        app.saveToLocalStorage();
-        $("#addInput").val("");
-        app.displayItem();
-    },
+        _getFromLocalStorage();
+        var tempInput = _addInput.value;
+        if (tempInput.length > 0) {
+            //wrzuć element do tablicy na koniec
+            app.items[app.items.length] = {};
+            app.items[app.items.length - 1].name = tempInput;
+            app.items[app.items.length - 1].isFinished = "false";
+            _saveToLocalStorage();
+            //wyzeruj wartość pola
+            _addInput.value = "";
+        } else {
+            alert("wpisz coś");
+        }
+        _displayItem();
+    };
 
-    editItem: function (e) {
-    //pobierz z local
-        app.getFromLocalStorage();
+    var _editItem = function (e) {
+        _getFromLocalStorage();
         var tableItemNumber = e.target.parentNode.id;
         var editItemValue = prompt("Edytuj");
         if (editItemValue.length === 0) {
             alert("Formularz jest pusty");
         }
         app.items[tableItemNumber].name = editItemValue;
-        app.saveToLocalStorage();
-        app.displayItem();
-    },
+        _saveToLocalStorage();
+        _displayItem();
+    };
 
-    deleteItem: function (e) {
-        app.getFromLocalStorage();
+    var _deleteItem = function (e) {
+        _getFromLocalStorage();
         //pobierz odpowiedni element tablicy
         var tableItemNumber = e.target.parentNode.id;
         app.items.splice(tableItemNumber, 1);
-        app.saveToLocalStorage();
-        app.displayItem();
-    }
-};
+        _saveToLocalStorage();
+        _displayItem();
+    };
 
-//event listeners
+    //cached dom items
+    var _list = document.getElementById("list");
+    var _addInput = document.getElementById("addInput");
+    var _addBtn = document.getElementById("addBtn");
+
+    return {
+        displayItem: _displayItem,
+        editItem: _editItem,
+        deleteItem: _deleteItem,
+        addItem: _addItem,
+        addBtn: _addBtn
+    }
+}());
 
 app.displayItem();
-$('#addBtn').on('click', app.addItem);
 
-$(document).on('change', '.itemSelect', function(e){
-    if (this.value === "erase"){
+app.addBtn.addEventListener("click", app.addItem);
+
+$(document).on("change", ".itemSelect", function (e) {
+    if (this.value === "erase") {
         app.deleteItem(e);
-    } else if (this.value === "edit"){
+    } else if (this.value === "edit") {
         app.editItem(e);
     }
 });
